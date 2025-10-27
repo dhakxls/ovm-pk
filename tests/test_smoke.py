a8ac7dc1010e5e5e397c91466fb468e94a8a16b7
dhakxls <bvomartin@gmail.com>
2025-10-27 09:14:31 -0400

    removed test_smoke.py

--- tests/test_smoke.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ["OVM_DRY_RUN"] = "1"
-
-from ovmpk.cli import pipeline_mvp
-
-def test_pipeline_smoke(tmp_path):
-    out = pipeline_mvp("configs/fast_test.yaml", "CYP3A4", "ketoconazole")
-    assert "results" in out
